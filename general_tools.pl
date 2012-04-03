74947b1037c8ddebec88662075ba9b63922199c9
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-03 17:39:56 -0400

    root_exec function needs work, otherwise looking good.

--- general_tools.pl
+++ general_tools.pl
@@ -3,16 +3,16 @@
 use strict;
 use warnings;
 
-my $username = "conor"; #This must be declared manually!
+my $username = "conor"; #Insert username to run commands as (e.g. synergyc, xrandr);
 
 sub root_exec {
-    my $command = shift;
-    system("/bin/su $username -c $command") == 0
-        or die "system command failed ($command): $?";
+    my @command = shift;
+    system("/bin/su $username -c '@command'") == 0
+        or die "system command failed (@command): $?";
 }
-sub log_this {
+sub logger {
     my $message = shift;
-    system("logger -s $message");
+    system("logger -s ActionByNetwork: '$message'");
 }
 
-
+1; #Since this script is reference in calls by other scripts, it must exit with True;
