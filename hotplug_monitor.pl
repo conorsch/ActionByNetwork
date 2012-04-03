f6c003b75aabf39a9792f80ecf7c3e8bc27d3a87
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-03 17:40:14 -0400

    Much cleaner. Still needs better root exec call.

--- hotplug_monitor.pl
+++ hotplug_monitor.pl
@@ -7,7 +7,8 @@ use strict;
 
 require qw/general_tools.pl/;
 
-system('export DISPLAY=:0.0'); #This is necessary because root won't be able to call xrandr without knowing the DISPLAY;
+#system("export DISPLAY=:0.0"); #This is necessary because root won't be able to call xrandr without knowing the DISPLAY;
+#`export DISPLAY=:0.0`; #This is necessary because root won't be able to call xrandr without knowing the DISPLAY;
 my $external_monitor = $ARGV[0] || "VGA1"; #Which monitor should be configured? Assume VGA1 if none;
 my %monitor_settings = (
         #If using KDE, grab next line from ~/.kde/share/config/krandrrc after using System Settings 
@@ -19,23 +20,32 @@ my %monitor_settings = (
 
 sub connect_monitor {
     my $monitor = shift;
-    my $check = `xrandr | grep $monitor`; #Grab output from xrandr that mentions whether monitor is connected;
-    chomp $check; #Probably necessary to remove trailing newline from $check variable;
-    if ($check =~ m/^$monitor connected/) { 
-        print "External monitor $monitor appears to be connected. Setting it up!\n";
-        `$root_exec 'xrandr --output LVDS1 --pos 2048x750 --mode 1366x768 --refresh 60.0186'\n
-            $root_exec 'xrandr --output VGA1 --pos 0x0 --mode 2048x1152 --refresh 59.9087'\n
-            $root_exec 'xrandr --output LVDS1 --primary'`; #Not sure whether it's necessary to root_exec all of these individually;
-    }
-    elsif ($check =~ m/^$display disconnected/) {
-        logger("External monitor $display does not appear to be connected; maintaining single display mode."); #do nothing
-    }
-    else { logger("ERROR: problem while trying to connect to an external monitor.");} #Also do nothing;
+    print "External monitor $monitor appears to be connected. Setting it up!\n";
+    my $command = $monitor_settings{$monitor};
+    root_exec("$command") == 0
+        or die "External monitor setup failed: $?";
+#    root_exec("xrandr --output LVDS1 --pos 2048x750 --mode 1366x768 --refresh 60.0186");
+#    root_exec("xrandr --output VGA1 --pos 0x0 --mode 2048x1152 --refresh 59.9087");
+#    root_exec("xrandr --output LVDS1 --primary"); #Not sure whether it's necessary to root_exec all of these individually;
 }
 
 sub disconnect_monitor {
-
-    system("xrandr --output $external_monitor --off");
+    my $monitor = shift;
+    logger("External monitor $monitor does not appear to be connected; maintaining single display mode."); #do nothing
+    system("xrandr --output $monitor --off");
 }
+
+my $check = `xrandr | grep $external_monitor`; #Grab output from xrandr that mentions whether monitor is connected;
+chomp $check; #Probably necessary to remove trailing newline from $check variable;
+
+if ($check =~ m/^$external_monitor connected/) { 
+    connect_monitor($external_monitor);
+}
+
+elsif ($check =~ m/^$external_monitor disconnected/) {
+    disconnect_monitor($external_monitor);
+}
+
+else { logger("ERROR: problem while trying to connect to an external monitor.");} #Also do nothing;
 
 1; #Since this script is reference in calls by other scripts, it must exit with True;
