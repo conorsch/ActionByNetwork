f4d3b38f0e3e7f8135f262a3f212f28851f6b752
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-05 15:43:04 -0400

    Added function to request possible network interfaces.

--- general_tools.pl
+++ general_tools.pl
@@ -5,14 +5,21 @@ use warnings;
 
 my $username = "conor"; #Insert username to run commands as (e.g. synergyc, xrandr);
 
-sub root_exec {
-    my @command = shift;
-    system("/bin/su $username -c '@command'") == 0
-        or die "system command failed (@command): $?";
-}
 sub logger {
     my $message = shift;
     system("logger -s ActionByNetwork: '$message'");
+}
+
+sub request_interfaces {
+    my @interfaces; #Set up a list to store interface values in;
+    my @ifconfig = `ifconfig`; #Grab output of ifconfig for parsing;
+    foreach my $line (@ifconfig) {
+        next if ($line =~ m/^\s/); #Interface names are left-justified, so ignore lines with leading whitespace;
+        chomp $line; #Remove trailing whitespace;
+        $line =~ s/(^\w*)(\s*.*$)/$1/; #Grab the anchored word, trash the rest of the line;
+        push @interfaces, $line unless $line eq "lo"; #Add interface to list, unless it's loopback;
+    }
+    return @interfaces; #Pass back results to function caller;
 }
 
 1; #Since this script is reference in calls by other scripts, it must exit with True;
