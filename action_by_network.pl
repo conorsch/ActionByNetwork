3160b2fcd7a4b37ad33c8bd2449d992400a09ecc
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-05 20:14:09 -0400

    Fixed typos, package implementation still incomplete.

--- action_by_network.pl
+++ action_by_network.pl
@@ -5,13 +5,10 @@ use strict;
 use warnings;
 use Moose;
 
-require qw/general_tools.pl monitor_setup.pl syneryg_setup.pl/; #Import necessary subroutines;
+require qw/general_tools.pl monitor_setup.pl synergy_setup.pl/; #Import necessary subroutines;
 
 my $interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
 my $status = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
-
-
-
 
 package Location {
 
@@ -22,11 +19,12 @@ package Location {
     my $at_home = Location->new();
     my $at_work = Location->new();
     my $on_the_road = Location->new();
+}
 
 sub retrieve_ssid {
     my $ssid = `iwgetid --raw`; #Grabs just SSID output, but with trailing newline (chomped below);
     chomp $ssid; #Necessary to remove trailing newline so string is pluggable in function calls;
-    logger("Action_by_Network script confirms network SSID to be: '$ssid'\n") if ($debugging == 1);
+    logger("Action_by_Network script confirms network SSID to be: '$ssid'\n");
     return $ssid; #Pass SSID back to function caller;
 }
 
