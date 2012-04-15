c2be4cbdab048ceb8d4247fd693f537485e7270d
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-15 10:56:31 -0400

    Strong parsing of hashes, even implemented a hashref2array function.

--- parse_config.pl
+++ parse_config.pl
@@ -12,10 +12,41 @@ my $config = YAML::Tiny->read( 'action_by_network.yml' );
 my $commands = $config->[0]->{home}->{commands};
 my $network = $config->[0]->{home}->{network};
 my $user = $config->[0]->{user};
+my $home_options = $config->[0]->{home};
 
+
+
+print ref($home_options) . "\n";
+print "The commands ref points to a: " . ref($commands) . "\n";
+print "The home_options ref points to a: " . ref($home_options) . "\n";
 #print "This is what ROOTTHING looks like: $root\n";
 print "This is what network looks like: $network\n";
 print "This is what user looks like: $user\n";
-print "This is what commands looks like: @$commands\n";
+print "This is what commands looks like: $commands\n";
+
+
+
+
+my @home_options = hashref2array($home_options);
+sub hashref2array {
+    my $hashref = shift;
+    my @array = (); #
+    foreach my $value (keys %$hashref) {
+        push @array,$value;
+    }
+    return @array;
+}
+
+
+print "This is what home_options looks like: @home_options";
+print "\n";
+
+my @home_commands = ();
+foreach my $command (sort keys %$commands) {
+    push @home_commands,$command;
+}
+print "This is what home_commands looks like: @home_commands";
+
+print "\n";
 
 
