ce5e77f32f3dfe8ff8f39142390b29d6b12ad8e0
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-16 16:38:24 -0400

    Implemented smart handling of determine_network location. Still need to remove debugging info.

--- parse_config.pl
+++ parse_config.pl
@@ -1,38 +1,49 @@
 #!/usr/bin/perl 
-#This script is a test for parsing a YAML config file.
+#This script performs a YAML configuration file for the ActionByNetwork suite of scripts.
+#It takes an SSID from the parent script (action_by_network.pl) and determines a location
+#that matches one specified in the configuration file. If it can't find one, it assumes roaming;
 use strict;
 use warnings;
-use YAML::Tiny;
+use YAML::Tiny; #Sufficient for parsing YAML configuration file;
 use diagnostics;
-#my $config_file = 'example_config.yml';
+use 5.12.0; #There are 'say' calls in here;
+#require qw/general_tools.pl/; #Get necessary boilerplate;
+
+#my $ssid = $ARGV[0] || retrieve_ssid(); #Get SSID from parent script; if none given, figure it out;
 my $ssid = "BloodOfNorsemen";
 
-my $config_file = 'action_by_network.yml';
-my $config = YAML::Tiny->read( $config_file );
+my $config_file = 'action_by_network.yml'; #This is the default name for a configuration file;
+#Better error-handling should be implemented, e.g. check ~/.abn/conf as well;
+my $config = YAML::Tiny->read( $config_file ); #Import config file as a hash reference;
 
-#my $root = $config->[0]->{rootproperty};
+#Let's start splitting up this hash reference into workable pieces;
 my $commands = $config->[0]->{home}->{commands};
 my $network = $config->[0]->{home}->{network};
 my $user = $config->[0]->{user};
 my $home_options = $config->[1]->{home};
+my $home_network = $config->[1]->{home}->{network};
 
-my $locations = $config->[1];
-my @locations = hashref2array($locations);
-print "My locations look like: @locations\n";
-print "Home options ref points to: " . ref($home_options) . "\n";
+sub determine_location { #Let's figure out where we're at; 
+    my $ssid = shift;
+    my $locations = $config->[1]; #Create hash reference from second section of conf file;
+    my @locations = hashref2array($locations); #Flatten hash reference into list;
+    foreach my $location (@locations) { #Iterate through list of locations in conf;
+        my $candidate_location = $config->[1]->{$location}->{network}; #Name possibility;
+        next unless $candidate_location eq $ssid; #Does our possibility match the ssid?;
+        return $candidate_location; #If so, pass it back!;
+    }
+    return "other"; #If we can't find the SSID in the configuration, assume roaming;
+}
 
+our $location = determine_location($ssid); #Figure out where we are;
+say "Location has been determined to be: $location";
+#logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
 
-my @home_options = hashref2array($home_options);
-sub hashref2array {
-    my $hashref = shift;
+sub hashref2array { #Handy function for flattening hash references into lists;
+    my $hashref = shift; #Unpack supplied hash reference;
     my @array = (); #
     foreach my $value (sort keys %$hashref) {
         push @array,$value;
     }
     return @array;
 }
-
-
-print "This is what home_options looks like: @home_options\n"; 
-
-
