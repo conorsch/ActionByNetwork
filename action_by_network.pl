ad628d104737b0e4724937375411b6edc5a397da
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-18 16:07:04 -0400

    Removed retrieve_ssid function, placed in general_tools.

--- action_by_network.pl
+++ action_by_network.pl
@@ -3,24 +3,11 @@
 #Call this script from /etc/NetworkManager/dispatcher.d/9action_by_network.sh
 use strict;
 use warnings;
-use Moose;
 
-require qw/general_tools.pl monitor_setup.pl synergy_setup.pl/; #Import necessary subroutines;
+require qw/general_tools.pl monitor_setup.pl synergy_setup.pl parse_config.pl/; #Import necessary subroutines;
 
 my $interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
 my $status = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
-
-sub retrieve_ssid {
-    my $ssid = `iwgetid --raw`; #Grabs just SSID output, but with trailing newline (chomped below);
-    chomp $ssid; #Necessary to remove trailing newline so string is pluggable in function calls;
-    while (!$ssid) {
-        sleep 5; #Wait a bit, giving the interface some time to finalize connection;
-        logger("Waiting another second for ssid...\n") #Useful feedback, not so necessary;
-        $ssid = retrieve_ssid(); #Try again!;
-    }
-    logger("Action_by_Network script confirms network SSID to be: '$ssid'\n");
-    return $ssid; #Pass SSID back to function caller;
-}
 
 my $ssid = retrieve_ssid(); #Get network name!
 my $location = ...; #Determine location by calling child script; currently parse_config;
