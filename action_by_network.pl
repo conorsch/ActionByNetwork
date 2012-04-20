7ff69f4a0f9d3c8022062d4730c1fb54fc33a3d7
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-20 15:44:23 -0400

    Added up/down logic and debugging output for this functionality.

--- action_by_network.pl
+++ action_by_network.pl
@@ -7,7 +7,7 @@ use 5.12.0;
 use File::Basename 'dirname'; #Since root will execute this script, we'll need to figure out where it's run from;
 
 my $cwd = dirname($0); #Get the current working directory. $0 is the same of the currently running script;
-my @required_scripts = qw/parse_config.pl/; #Declare required scripts here, so fullpaths can be grabbed;
+my @required_scripts = qw/parse_config.pl general_tools.pl/; #Declare required scripts here, so fullpaths can be grabbed;
 foreach (@required_scripts) { #Let's look at all the scripts declared as required above;
     s/(^.*$)/$cwd\/$1/; #Stitch together the path and the name of the required script;
     require $_; #State the requirement;
@@ -16,12 +16,19 @@ foreach (@required_scripts) { #Let's look at all the scripts declared as require
 
 my $network_interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
 my $connection_up_or_down = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
+logger("CONNECTION STATUS LOOKS LIKE: $connection_up_or_down");
 
-my $ssid = retrieve_ssid(); #Get network name!
-our $location = determine_location($ssid); #Figure out where we are;
-say "Location has been determined to be: $location";
-logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
-my @commands = find_commands($location); #Get list of commands to be run for present location;
-run_commands(@commands); #Run necessary commands; 
+if ($connection_up_or_down eq "up") {
+
+    my $ssid = retrieve_ssid(); #Get network name!
+    our $location = determine_location($ssid); #Figure out where we are;
+    say "Location has been determined to be: $location";
+    logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
+    my @commands = find_commands($location); #Get list of commands to be run for present location;
+    run_commands(@commands); #Run necessary commands; 
+}
+else {
+    logger("Network connection terminated, not taking any action.");
+}
 
 
