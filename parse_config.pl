72f087fb5c76fa1ffebf930e4f5b64fdcd07f1ba
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-19 22:33:05 -0400

    Refactored require statements, revised synergy call.

--- parse_config.pl
+++ parse_config.pl
@@ -7,9 +7,16 @@ use warnings;
 use YAML::Tiny; #Sufficient for parsing YAML configuration file;
 use diagnostics;
 use 5.12.0; #There are 'say' calls in here;
-require qw/general_tools.pl synergy_setup.pl/; #Get necessary boilerplate;
+use File::Basename 'dirname'; #Since root will execute this script, we'll need to figure out where it's run from;
 
-my $ssid = $ARGV[0] || retrieve_ssid(); #Get SSID from parent script; if none given, figure it out;
+my $cwd = dirname($0); #Get the current working directory. $0 is the same of the currently running script;
+my @required_scripts = qw/general_tools.pl synergy_setup.pl/; #Declare required scripts here, so fullpaths can be grabbed;
+foreach (@required_scripts) { #Let's look at all the scripts declared as required above;
+    s/(^.*$)/$cwd\/$1/; #Stitch together the path and the name of the required script;
+    require $_; #State the requirement;
+}
+
+our $location;
 
 my $config_file = 'action_by_network.yml'; #This is the default name for a configuration file;
 #Better error-handling should be implemented, e.g. check ~/.abn/conf as well;
@@ -28,17 +35,10 @@ sub determine_location { #Let's figure out where we're at;
     return "other"; #If we can't find the SSID in the configuration, assume roaming;
 }
 
-our $location = determine_location($ssid); #Figure out where we are;
-say "Location has been determined to be: $location";
-logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
-my @commands = find_commands($location); #Get list of commands to be run for present location;
-run_commands(@commands); #Run necessary commands; 
-
 sub find_commands { #Once location is known, next step is to build up commands;
     my $location = shift; #Unpack location, supplied by function caller;
     my $commands_to_run = $config->[1]->{$location}->{commands}; #Create hash reference from second section of conf file;
     my @commands_to_run = hashref2array($commands_to_run); #Flatten hash reference into list;
-    say "These are the commands that should be run: @commands_to_run";
     return @commands_to_run; #Pass back list of commands to run;
 }
 
@@ -47,11 +47,9 @@ sub run_commands {
     foreach my $command (@commands) { #Look at each command;
         given ($command) { #
             when (/synergy/) { 
-                my $target_host = $config[1]->{$location}->{commands}->{synergy}->{ip};    
-                synergy_setup.pl($target_host);  
-            }
-            when (/tunnel/) { 
-                ...; 
+                my $target_host = $config->[1]->{$location}->{commands}->{synergy}->{ip};    
+                say "Found the synergy target! It is: $target_host";
+                start_synergy($target_host);  
             }
         }
     }
