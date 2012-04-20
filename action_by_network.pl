83a54a4dc5cbad67f1f8f2dc86cb2febf3fb7666
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-20 16:06:10 -0400

    Added comments for features to come, cleaned up debugging code.

--- action_by_network.pl
+++ action_by_network.pl
@@ -13,22 +13,22 @@ foreach (@required_scripts) { #Let's look at all the scripts declared as require
     require $_; #State the requirement;
 }
 
-
 my $network_interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
 my $connection_up_or_down = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
-logger("CONNECTION STATUS LOOKS LIKE: $connection_up_or_down");
 
-if ($connection_up_or_down eq "up") {
-
+if ($connection_up_or_down eq "up") { #If the network connection is being established...;
     my $ssid = retrieve_ssid(); #Get network name!
-    our $location = determine_location($ssid); #Figure out where we are;
-    say "Location has been determined to be: $location";
+    our $location = determine_location($ssid); #Figure out where we are, based on SSID;
+    say "Location has been determined to be: $location"; #Debugging feedback, will remove later;
     logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
     my @commands = find_commands($location); #Get list of commands to be run for present location;
     run_commands(@commands); #Run necessary commands; 
 }
-else {
+if ($connection_up_or_down eq "down") { #If the network connection is being terminated...;
     logger("Network connection terminated, not taking any action.");
+    ...; #Shutdown procedures such as removing connections should be implemented here;
 }
 
-
+else {
+    logger("Network connection status could not be established; taking no action.";
+}
