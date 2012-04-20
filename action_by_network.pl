f2e37c9e1dd6d23bcdf2e89e6fd4309cc64f1d01
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-19 22:32:08 -0400

    Reconfigured require statement, removed unimplemented location logic.

--- action_by_network.pl
+++ action_by_network.pl
@@ -3,16 +3,25 @@
 #Call this script from /etc/NetworkManager/dispatcher.d/9action_by_network.sh
 use strict;
 use warnings;
+use 5.12.0;
+use File::Basename 'dirname'; #Since root will execute this script, we'll need to figure out where it's run from;
 
-require qw/general_tools.pl monitor_setup.pl synergy_setup.pl parse_config.pl/; #Import necessary subroutines;
+my $cwd = dirname($0); #Get the current working directory. $0 is the same of the currently running script;
+my @required_scripts = qw/parse_config.pl/; #Declare required scripts here, so fullpaths can be grabbed;
+foreach (@required_scripts) { #Let's look at all the scripts declared as required above;
+    s/(^.*$)/$cwd\/$1/; #Stitch together the path and the name of the required script;
+    require $_; #State the requirement;
+}
+
 
 my $network_interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
 my $connection_up_or_down = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
 
 my $ssid = retrieve_ssid(); #Get network name!
-my $location = ...; #Determine location by calling child script; currently parse_config;
-given ($location) {
-    when (home)     {...} #Call necessary child scripts;
-    when (work)     {...} #Call necessary child scripts;
-    ...
-}
+our $location = determine_location($ssid); #Figure out where we are;
+say "Location has been determined to be: $location";
+logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
+my @commands = find_commands($location); #Get list of commands to be run for present location;
+run_commands(@commands); #Run necessary commands; 
+
+
