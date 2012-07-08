a1227c157c14b6efc6ec8f528b3fcf56c56273d8
Conor Schaefer <conor.schaefer@gmail.com>
2012-07-07 21:03:31 -0400

    Refactored main logic, reads more simply now.

--- action_by_network.pl
+++ action_by_network.pl
@@ -3,33 +3,40 @@
 #Call this script from /etc/NetworkManager/dispatcher.d/9action_by_network.sh
 use strict;
 use warnings;
-use 5.12.0;
-use File::Basename 'dirname'; #Since root will execute this script, we'll need to figure out where it's run from;
+use feature qw/switch say/; #case statements and say is better than print;
+use ActionByNetwork;
 
-my $cwd = dirname($0); #Get the current working directory. $0 is the same of the currently running script;
-my @required_scripts = qw/parse_config.pl general_tools.pl/; #Declare required scripts here, so fullpaths can be grabbed;
-foreach (@required_scripts) { #Let's look at all the scripts declared as required above;
-    s/(^.*$)/$cwd\/$1/; #Stitch together the path and the name of the required script;
-    require $_; #State the requirement;
-}
+#grab interface (e.g. wlan0) and direction of connection (e.g. up, down) from NetworkManager;
+my ($network_interface, $state_change) = @ARGV;
+#my $location; #initialize variable in global scope;
 
-my $network_interface = $ARGV[0]; #grab connection interface (e.g. wlan0) from NetworkManager as first argument passed;
-my $connection_up_or_down = $ARGV[1]; #grab connection status (e.g. up, down) from NetworkManager as second argument passed;
+given ($state_change) { #Examine network connection status (up or down);
 
-given ($connection_up_or_down) { #Examine network connection status (up or down);
     when (/up/) { #If the network connection is being established...;
-        my $ssid = retrieve_ssid(); #Get network name!
-        our $location = determine_location($ssid); #Figure out where we are, based on SSID;
-        say "Location has been determined to be: $location"; #Debugging feedback, will remove later;
-        logger("Location has been determined to be: $location"); #Perhaps this should be included in function?;
-        my @commands = find_commands($location); #Get list of commands to be run for present location;
-        run_commands(@commands); #Run necessary commands; 
+
+        given ($network_interface) { #examine interface for active connection;
+
+            when (/(wlan|ath)\d+/) { #if active connection appears to be wifi;
+                my $location = ActionByNetwork::determine_location; #figure out where we are, based on SSID;
+                ActionByNetwork::run_commands($location); #run commands returned by find_commands; 
+            }
+
+            when (/eth\d+/) { #if active connection appears to be Ethernet;
+                return; #do nothing;
+            }
+
+            default { #assume ethernet connection;
+                return; #do nothing;
+            }
+        }
     }
+
     when (/down/) { #If the network connection is being terminated...;
-        logger("Network connection terminated, not taking any action.");
-        ...; #Shutdown procedures such as removing connections should be implemented here;
+        ActionByNetwork::logger("Network connection terminated, killing synergyc...");
+        ActionByNetwork::cleanup_commands; #kill any dangling processes as network goes down;
     }
-    default {
-        logger("Network connection status could not be established; taking no action.");
+
+    default { #failsafe in case input is not properly given;
+        ActionByNetwork::logger("Network connection status could not be established; taking no action.");
     }
 }
