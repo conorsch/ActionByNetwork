c3244684b74240e622f67a4c202c31202ea667bc
Conor Schaefer <conor.schaefer@gmail.com>
2012-07-07 21:04:16 -0400

    Started moving subroutines from general_tools to module.

---
+++ ActionByNetwork.pm
@@ -0,0 +1,154 @@
+=head1 NAME
+
+ActionByNetwork - Perl module for automating actions according to network state
+
+=head1 SYNOPSIS
+
+use ActionByNetwork;
+
+=head1 DESCRIPTION
+
+Creates and maintains dotfile of user preferences for notifications about quota usage.
+
+Methods to build list of valid home directories, update user preferences, and retrieve 
+user preferences. Supports customizing frequency of notications and percent of quota 
+usage that generates email. 
+
+=head2 Methods
+
+=over 4
+
+=item * get_users([$home_pattern])
+
+Walks through the /etc/passwd file and builds a list of valid home directories. 
+Function supports argument of pattern to identify valid homes. If none is given,
+pattern defaults to '^/home[0-9]+/', to match SEAS user home directories.
+
+The function returns two hash references, with the structures: 
+
+#===============================================================================
+=cut
+#!/usr/bin/perl
+package ActionByNetwork; #name our package;
+use strict;
+use warnings;
+use feature qw/switch say/; #case statements and say is better than print;
+use Config::Simple; #for configuration file processing;
+use YAML::Tiny;
+
+my $config_file = '/home/conor/gits/ActionByNetwork/action_by_network.yml';
+my $config = YAML::Tiny->read( $config_file ) #Import config file as a hash reference;
+    or die "Unable to read configuration file at '$config_file'";
+
+sub retrieve_ssid { #return SSID of current wireless connection;
+    my $ssid = `/sbin/iwgetid --raw`; #capture SSID from iwgetid system call;
+    while (!$ssid) { #if $ssid wasn't found...;
+        sleep 5; #wait a bit, giving the interface some time to finalize connection;
+        logger("Determining SSID for current connection ($ssid)...\n"); #provide feedback;
+        my $ssid = `/sbin/iwgetid --raw`; #capture SSID from iwgetid system call;
+    }
+    chomp $ssid; #remove pesky trailing newline;
+    logger("Action_by_Network script confirms network SSID to be: '$ssid'\n"); #provide feedback;
+    return $ssid; #pass SSID back to function caller;
+}
+
+sub retrieve_device { #return device name for active network connection;
+    `/usr/bin/nm-online -t 1`; #run check (returns 0 for success, 1 for failure);
+    if ( $? == 0 ) { #check whether connection check reported success;
+        my @device_check = `/usr/bin/nmcli dev`; #grab list of devices managed by NetworkManager;
+        foreach my $line (@device_check) { #look at output from `nmcli dev` check;
+            chomp $line; #remove pesky trailing newline;
+            next unless $line =~ m/connected/; #skip unless line contains "connected";
+            my @line = split('\s', $line); #split current line on whitespace;
+            my $device = shift @line; #grab first column of this line, the device name;
+            return $device; #take first connected device found;
+        }
+    }
+    else { #if online check failed;
+        return; #return failure;
+    }
+}
+
+sub logger { #write to /var/log/syslog using 'logger -s';
+    my $message = shift; #Grab string to be logged from function caller;
+    system("logger -s ActionByNetwork: '$message'"); #Send to system log with ActionByNetwork prefixed;
+}
+
+sub determine_location { #return current location by looking up SSID in conf file;
+    my $ssid = retrieve_ssid; #get ssid from subroutine;
+    my $locations = $config->[1]; #create hash reference from second section of conf file;
+    my $location; #initialize variable in proper scope;
+    if ( exists $locations->{$ssid} ) { #if current SSID exists in configuration file;
+        $location = $locations->{$ssid}; #retrieve location for this SSID;
+    }
+    else { #if SSID is not found in conf file;
+        $location = 'other'; #assume roaming and report location as 'other';
+    }
+
+    logger("Location has been determined to be '$location'");
+    return $location; #pass current location back to function caller;
+}
+sub parse_config { #read in config file for ActionByNetwork; 
+    my $config_file = shift;
+    my $config = YAML::Tiny->read( $config_file ); #Import config file as a hash reference;
+}
+
+##### Cleanup commands, for when network goes down and processes should be killed;
+sub cleanup_commands { #kill any processes before network goes down;
+    my @commands_to_kill = qw/synergyc/;
+    foreach my $command (@commands_to_kill) { #look at each command in the list;
+        system("/usr/bin/killall", $command) unless (!$command); #Ensure that no conflicting Synergy client instances are running (unless there isn't one);
+    }
+}
+
+sub check_network_state { #Find out whether there is currently an active network connection;
+    my $check = `/usr/bin/nmcli nm | tail -n 1 | awk '{print $2}'`; #Find out whether connected;
+    return 1 if $check eq "connected"; #If 'connected' is found, then report True; 
+    return; #Otherwise, bare return (false);
+}
+ 
+
+sub get_commands { #return list of commands to be run for current location;
+    my $location = shift; #unpack location from function caller;
+    my $commands_to_run = $config->[1]->{$location}->{commands}; #build hashreffrom second section of conf file;
+    my @commands_to_run; #initialize array in proper scope;
+    foreach my $value (sort keys %$commands_to_run) { #Sort keys, iterate through values;
+        push @commands_to_run,$value; #Add that value to the flatted list created above;
+    }
+    return @commands_to_run; #Pass back list of commands to run;
+}
+
+sub run_commands { #run specified commands according to config file;
+    my $location = shift; #unpack location from function caller;
+    my @commands = get_commands($location); #unpack list of commands to run from caller;
+    foreach my $command (@commands) { #look at each command in the list passed in;
+        given ($command) { #begin case statement for commands;
+
+            when (/synergy/) { #if specified command is 'synergy';
+                my $target_host = $config->[1]->{$location}->{commands}->{synergy}->{ip};    
+                logger("Starting synergyc...");
+                system('/usr/bin/synergyc', $target_host); #start synergyc, connecting to host;
+            }
+
+            when (/monitor/) { #if specified command is 'monitor';
+                system("monitor_setup.pl");
+                logger("TESTING MONITOR ACTION NOW");
+            }
+
+            when (/custom/) { #if user has specified command in conf file;
+                my $custom_command = $config->[1]->{$location}->{commands}->{custom};    
+                given ($custom_command) { #analyze user-specified command;
+                    when (/rm /) { #if it looks like there's a remove command;
+                        die "Unable to execute custom command '$custom_command'; Looks like rm!";
+                    }
+                    when (/^\//) { #if command does not begin with '/';
+                        die "Unable to execute custom command '$custom_command'; Specify fullpath in conf file! ";
+                    }
+                    default { #if command looks safe;
+                        system("$command"); #run user-specified command;
+                    }
+                }
+            }
+        }
+    }
+}
--- general_tools.pl
+++ general_tools.pl
@@ -16,29 +16,6 @@ sub check_depends { #Function to ensure the supplied binaries are present on sys
     return @missing_packages if exists $missing_packages[0]; #Return array if not empty;
 }
 
-sub check_network_state { #Find out whether there is currently an active network connection;
-    my $check = `nmcli nm | tail -n 1 | awk '{print $2}'`; #Find out whether connected;
-    return 1 if $check eq "connected"; #If 'connected' is found, then report True; 
-    return; #Otherwise, bare return (false);
-}
-
-sub retrieve_ssid {
-    my $ssid = run_as_user($username, 'iwgetid --raw'); #Grabs just SSID output, but with trailing newline (chomped below);
-    while (!$ssid) { #If $ssid wasn't found...;
-        sleep 5; #Wait a bit, giving the interface some time to finalize connection;
-        logger("Determining SSID for current connection ($ssid)...\n"); #Provide feedback;
-        my $ssid = run_as_user($username, 'iwgetid --raw'); #Grabs just SSID output, but with trailing newline (chomped below);
-    }
-    chomp $ssid; #Necessary to remove trailing newline so string is pluggable in function calls;
-    logger("Action_by_Network script confirms network SSID to be: '$ssid'\n"); #Provide feedback;
-    return $ssid; #Pass SSID back to function caller;
-}
-
-sub logger {
-    my $message = shift; #Grab string to be logged from function caller;
-    system("logger -s ActionByNetwork: '$message'"); #Send to system log;
-}
-
 sub run_as_user {
     my ($username, $command) = @_; #Grab both username to run as and command to run from function caller;
     my $result = `/bin/su $username -c '$command'`; #Run command, grabbing output just in case;
